6b020de4d1c7a594d1dcf7ce917f4b5e225e8142
Dalton Hildreth <djdjhildreth@gmail.com>
2017-10-22 22:50:43 -0500

    Add index buffer objects
    'Forgot' this in the f/triangle. Going to create meshes now.

--- src/render.cpp
+++ src/render.cpp
@@ -20,22 +20,30 @@ Mesh tri;
 void draw_init() {
     //test that all functions can work before moving it off into a mesh.
     //gotta be incremental about this!
-    const float mesh_data[3*3] {
-        -.5f, -.5f, .0f,
+    const float mesh_vertices[4*3] {
+         .5f,  .5f, .0f,
          .5f, -.5f, .0f,
-         .0f,  .5f, .0f
+        -.5f,  .5f, .0f,
+        -.5f, -.5f, .0f
+    };
+    const uint mesh_indices[2*3] {
+        0, 1, 2,
+        1, 2, 3
     };
 
     //generate
     glGenBuffers(1, &tri.VBO);
+    glGenBuffers(1, &tri.IBO);
     glGenVertexArrays(1, &tri.VAO);
 
     //bind
     glBindVertexArray(tri.VAO);
     glBindBuffer(GL_ARRAY_BUFFER, tri.VBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, tri.IBO);
 
     //buffer & interpret
-    glBufferData(GL_ARRAY_BUFFER, sizeof(mesh_data), mesh_data, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(mesh_vertices), mesh_vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(mesh_indices), mesh_indices, GL_STATIC_DRAW);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
     glEnableVertexAttribArray(0);
 
@@ -55,5 +63,5 @@ void draw() {
 
     tricolor.use();
     glBindVertexArray(tri.VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 }
