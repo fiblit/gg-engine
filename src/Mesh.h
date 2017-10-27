85963eda9656db1619265e8198a58e696f5c246f
Dalton Hildreth <djdjhildreth@gmail.com>
2017-10-27 01:50:15 -0500

    Add Mesh class
    This can handle textures, shaders ("materials"), different mesh data,
    and drawing.
    
    Also, the mesh data has to have vertices of position, normal, tex-coord
    for right now.
    
    Interesting note: I learned you can bind textures within a VAO and it will
    still work. :)

---
+++ src/Mesh.h
@@ -0,0 +1,54 @@
+#ifndef MESH_H
+#define MESH_H
+
+#include <glad.h>
+#include <memory>
+#include <vector>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include "Shader.h"
+
+struct Vertex {
+    glm::vec3 pos;
+    glm::vec3 norm;
+    glm::vec2 tex;
+};
+
+enum class Texmap {
+    other, ambient, diffuse, specular
+};
+std::string texmap_to_string(Texmap t);
+
+struct Texture {
+    GLuint id;
+    Texmap type;
+};
+
+class Mesh {
+public:
+    Mesh(const std::vector<Vertex>& vertices,
+        const std::vector<GLuint>& indices,
+        const std::vector<Texture>& textures);
+    void set_material(std::shared_ptr<Shader> material);
+    void draw();
+private:
+    //helpers
+    void gen();
+    void bind();
+    void bind_textures();
+    void attrib();
+    void buffer();
+
+    //mesh
+    std::vector<Vertex> vertices;
+    std::vector<GLuint> indices;
+    std::vector<Texture> textures;
+
+    //OpenGL buffers and ids
+    GLuint vao;
+    GLuint vbo;
+    GLuint ibo;
+    std::shared_ptr<Shader> mat;
+};
+
+#endif //MESH_H
